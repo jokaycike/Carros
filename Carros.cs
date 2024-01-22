190738410516b932fa8178fc5045fbbdea940364
Joana Luiza <joanasilva@uniaraxa.edu.br>
2024-01-22 08:34:37 -0300

    Meu primeiro commit

---
+++ Carros.cs
@@ -0,0 +1,240 @@
+﻿
+namespace ConsoleApp1
+{
+    internal class teste
+    {
+        public struct Carro
+        {
+            public string placa;
+            public string modelo;
+            public int ano;
+            public string cor;
+        }
+
+        static void Main(string[] args)
+        {
+
+            const int TAM = 2;
+            Carro[] c = new Carro[TAM];
+            int op;
+            bool sair = false;
+            while (sair == false)
+            {
+                Console.Clear();
+                Console.WriteLine("1 - Cadastrar Carro");
+                Console.WriteLine("2 - Consultar Carro por Ano de Fabricação");
+                Console.WriteLine("3 - Consultar Carro por Modelo");
+                Console.WriteLine("4 - Consultar Carro por Cor");
+                Console.WriteLine("5 - Exibir todos os carros cadastrados");
+                Console.WriteLine("6 - Alterar algum dado de um carro");
+                Console.WriteLine("7 - Excluir um carro");
+                Console.WriteLine("0 - Sair");
+                Console.Write("\nEscolha uma opção: ");
+                op = int.Parse(Console.ReadLine());
+                switch (op)
+                {
+                    case 1:
+                        CadastroCarro(c);
+                        break;
+                    case 2:
+                        ConsultaCarroAnoFab(c);
+                        break;
+                    case 3:
+                        ConsultaCarroModelo(c);
+                        break;
+                    case 4:
+                        ConsultaCarroCor(c);
+                        break;
+                    case 5:
+                        ExibirTodosCarrosCadastrados(c);
+                        break;
+                    case 6:
+                        AlterarInfCarro(c);
+                        break;
+                    case 7:
+                        ExcluirCarro(c);
+                        break;
+                    case 0:
+                        Console.Write("\nPressione qualquer tecla para sair...");
+                        Console.ReadKey();
+                        sair = true;
+                        break;
+                }
+            }
+        }
+
+        //Metódos --->
+
+        static void CadastroCarro(Carro[] c)
+        {
+            const int TAM = 2;
+            Console.Clear();
+            Console.WriteLine("***** Cadastramento de Carro *****\n");
+            for (int i = 0; i < TAM; i++)
+            {
+                Console.Write("Qual é a placa do carro? ");
+                c[i].placa = Console.ReadLine();
+                Console.Write("Qual o modelo do carro? ");
+                c[i].modelo = Console.ReadLine();
+                Console.Write("Qual o ano do carro? ");
+                c[i].ano = int.Parse(Console.ReadLine());
+                Console.Write("Qual é a cor do carro? ");
+                c[i].cor = Console.ReadLine();
+            }
+            Console.Write("\nPressioneEnter para retornar ao menu principal");
+            Console.ReadKey();
+        }
+
+
+        static void ConsultaCarroAnoFab(Carro[] c)
+        {
+            int ano;
+            const int TAM = 2;
+            Console.Clear();
+            Console.WriteLine("***** Consulta por ano de fabricação *****\n");
+            Console.Write("Informe o ano desejado com 4 dígitos: ");
+            ano = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            for (int i = 0; i < TAM; i++)
+            {
+                Console.WriteLine("Placa / Modelo / Ano / Cor");
+                if (c[i].ano == ano)
+                {
+                    Console.WriteLine(c[i].placa + " " + c[i].modelo + " " + (c[i].ano).ToString() + " " + c[i].modelo);
+                }
+            }
+            Console.Write("\nPressioneEnter para retornar ao menu principal");
+            Console.ReadKey();
+        }
+
+        static void ConsultaCarroModelo(Carro[] c)
+        {
+            string modelo;
+            const int TAM = 2;
+            Console.Clear();
+            Console.WriteLine("***** Consulta por ano de fabricação *****\n");
+            Console.Write("Informe o modelo desejado: ");
+            modelo = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Modelo / Ano / Cor");
+            for (int i = 0; i < TAM; i++)
+            {
+                if (c[i].modelo == modelo)
+                {
+                    Console.WriteLine(c[i].placa + " " + c[i].modelo + " " + (c[i].ano).ToString() + " " + c[i].cor);
+                }
+            }
+            Console.Write("\nPressioneEnter para retornar ao menu principal");
+            Console.ReadKey();
+        }
+
+
+        static void ConsultaCarroCor(Carro[] c)
+        {
+            string cor;
+            const int TAM = 2;
+            Console.Clear();
+            Console.WriteLine("***** Consulta por cor do veículo *****\n");
+            Console.Write("Informe a cor desejada: ");
+            cor = Console.ReadLine();
+            Console.WriteLine();
+            for (int i = 0; i < TAM; i++)
+            {
+                Console.WriteLine("Placa / Modelo / Ano / Cor");
+                if (c[i].cor == cor)
+                {
+                    Console.WriteLine(c[i].placa + " " + c[i].modelo + " " + (c[i].ano).ToString() + " " + c[i].modelo);
+                }
+            }
+            Console.Write("\nPressioneEnter para retornar ao menu principal");
+            Console.ReadKey();
+        }
+
+
+        static void ExibirTodosCarrosCadastrados(Carro[] c)
+        {
+            const int TAM = 2;
+            Console.Clear();
+            Console.WriteLine("Placa / Modelo / Ano / Cor");
+            for (int i = 0; i < TAM; i++)
+            {
+                Console.WriteLine(c[i].placa + " " + c[i].modelo + " " + (c[i].ano).ToString() + " " + c[i].cor);
+            }
+            Console.Write("\nPressioneEnter para retornar ao menu principal");
+            Console.ReadKey();
+        }
+
+
+        static void AlterarInfCarro(Carro[] c)
+        {
+            const int TAM = 2;
+            string placa;
+            int op;
+            Console.Clear();
+            Console.Write("Informe a placa do carro (formato AAA9999): ");
+            placa = Console.ReadLine();
+            for (int i = 0; i < TAM; i++)
+            {
+                if (c[i].placa == placa)
+                {
+                    Console.WriteLine(c[i].modelo + " " + (c[i].ano).ToString() + " " + c[i].cor);
+                    Console.Write("Qual dado do veículo deseja alterar (1 - placa, 2 - modelo, 3 - cor, 4 - ano de fabricação)? ");
+                    op = int.Parse(Console.ReadLine());
+                    switch (op)
+                    {
+                        case 1:
+                            Console.Write("Nova placa? ");
+                            c[i].placa = Console.ReadLine();
+                            break;
+                        case 2:
+                            Console.Write("Novo modelo? ");
+                            c[i].modelo = Console.ReadLine();
+                            break;
+                        case 3:
+                            Console.Write("Nova cor? ");
+                            c[i].cor = Console.ReadLine();
+                            break;
+                        case 4:
+                            Console.Write("Novo ano de fabricação? ");
+                            c[i].ano = int.Parse(Console.ReadLine());
+                            break;
+                        default:
+                            Console.WriteLine("Opçãoinválida");
+                            break;
+                    }
+                    break;
+                }
+                else
+                    Console.WriteLine("Placa não encontrada\n");
+            }
+            Console.Write("\nPressioneEnter para retornar ao menu principal");
+            Console.ReadKey();
+        }
+
+
+        static void ExcluirCarro(Carro[] c)
+        {
+            string placa;
+            const int TAM = 2;
+            Console.Clear();
+            Console.WriteLine("***** Consulta por placa *****\n");
+            Console.Write("Informe a placa desejada: ");
+            placa = Console.ReadLine();
+            Console.WriteLine();
+            for (int i = 0; i < TAM; i++)
+            {
+                Console.WriteLine("Placa / Modelo / Ano / Cor");
+                if (c[i].placa == placa)
+                {
+                    c[i].placa = "";
+                    c[i].modelo = "";
+                    c[i].ano = 0;
+                    c[i].cor = "";
+                    Console.WriteLine("Veículo excluído!");
+                }
+            }
+            Console.Write("\nPressioneEnter para retornar ao menu principal");
+            Console.ReadKey();
+        }
+    }
+}
